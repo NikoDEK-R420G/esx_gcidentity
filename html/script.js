f5bc16ea40a9f40a247352ecaf1399925ef49d3c
NikoDEK <33868839+NikoDEK-R420G@users.noreply.github.com>
2017-11-30 07:24:53 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -18,70 +18,40 @@ GUIAction = {
             data.taille = data.taille + ' cm'
         }
 		if (data.jobs !== undefined){
-            if (data.jobs == "unemployed")
-				data.jobs = "🚬Chômeur"
+                if (data.jobs == "unemployed")
+				data.jobs = "💼Chômeur"
 			else if (data.jobs == "slaughterer")
-				data.jobs = "🔪Abatteur"
+				data.jobs = "🐓Abatteur"
 			else if (data.jobs == "fisherman")
-				data.jobs = "🐟Pêcheur"
+				data.jobs = "🎣Pêcheur"
 			else if (data.jobs == "miner")
 				data.jobs = "⛏️Mineur"
 			else if (data.jobs == "lumberjack")
-				data.jobs = "🌳Bûcheron"
+				data.jobs = "🌲Bûcheron"
 			else if (data.jobs == "fuel")
 				data.jobs = "⛽Raffineur"
 			else if (data.jobs == "reporter")
-				data.jobs = "Journaliste"
-			else if (data.jobs == "textil")
-				data.jobs = "👕Couturier"
-			else if (data.jobs == "cardealer")
-				data.jobs = "Concessionnaire"
+				data.jobs = "🎥CNN"
 			else if (data.jobs == "police")
 				data.jobs = "👮LSPD"
 			else if (data.jobs == "banker")
-				data.jobs = "Banquier"
-			else if (data.jobs == "🚕 Taxi")
-				data.jobs = "Taxi"
-			else if (data.jobs == "realestateagent")
-				data.jobs = "Agent immobilier"
+				data.jobs = "🏦Banquier"
+			else if (data.jobs == "taxi")
+				data.jobs = "🚖Taxi"
 			else if (data.jobs == "mecano")
-				data.jobs = "👨‍🔧Mécano"
+				data.jobs = "🔧Mécano"
 			else if (data.jobs == "ambulance")
-				data.jobs = "🚑Ambulance"
-			else if (data.jobs == "gouv")
-				data.jobs = "assureur"
-			else if (data.jobs == "Assureur")
-				data.jobs = "Gouvernement"
-			else if (data.jobs == "brewer")
-				data.jobs = "🍺Brasseur"
-			else if (data.jobs == "winegrowe")
-				data.jobs = "🍷Vigneron"
-			else if (data.jobs == "airlines")
-				data.jobs = "Pilote aérien"
-			else if (data.jobs == "disco")
-				data.jobs = "Disco"
-			else if (data.jobs == "bus")
-				data.jobs = "🚌Bus"
-			else if (data.jobs == "trucker")
-				data.jobs = "Chauffeur"
-			else if (data.jobs == "airdealer")
-				data.jobs = "Vendeur aviation"
-			else if (data.jobs == "fermier")
-				data.jobs = "👨‍🌾Fermier"
-			else if (data.jobs == "banksecurity")
-				data.jobs = "🔑Security Oasis"
-			else if (data.jobs == "pizza")
-				data.jobs = "🍕Pizza"
-			else if (data.jobs == "gouv")
-				data.jobs = "garbage"
-			else if (data.jobs == "gouv")
-				data.jobs = "🗑️Eboueur"
-			else if (data.jobs == "fbi")
-				data.jobs = "FBI"
+				data.jobs = "🚑LSMC"
+			else if (data.jobs == "brassier")
+				data.jobs = "🍺Brassier"
+			else if (data.jobs == "vigneron")
+				data.jobs = "🍇Vigneron"
 			else if (data.jobs == "unicorn")
 				data.jobs = "🦄Unicorn"
-			else if (data.jobs == "state")
-				data.jobs = "State"
+			else if (data.jobs == "foodtruck")
+				data.jobs = "🍔Foodtruck"
+			else if (data.jobs == "brinks")
+				data.jobs = "💵Brinks"
         }
         ['nom','prenom','jobs', 'dateNaissance', 'sexe', 'taille'].forEach(k => {
             $('#'+k).text(data[k] || infoMissing)
